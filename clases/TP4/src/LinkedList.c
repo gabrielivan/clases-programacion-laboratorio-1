6d62687121dd63b89f56e7ad96c2056bb82f5b5b
gabrielivan <40808771+gabrielivan@users.noreply.github.com>
2018-11-06 19:08:33 -0300

    Add files via upload

--- clases/TP4/src/LinkedList.c
+++ clases/TP4/src/LinkedList.c
@@ -94,14 +94,14 @@ static int addNode(LinkedList* this, int nodeIndex,void* pElement) //size de la 
     int returnAux = -1;
     Node* newNode = malloc(sizeof(Node));
 
-    if(this != NULL && nodeIndex >= 0 && nodeIndex <= ll_len(this) )
+    if(this != NULL && nodeIndex >= 0 && nodeIndex <= ll_len(this))
     {
        //estoy en condiciones de agregar un nodo.
        //para agregar un nodop tengo que preguntar en que posicion esta.
         if(this->pFirstNode == NULL && nodeIndex == 0)//D
         {
             //agregar el primer nodo.
-           this->pFirstNode = newNode;
+            this->pFirstNode = newNode;
             newNode->pElement = pElement;
             this->size = ll_len(this) + 1;
             returnAux = 0;
@@ -112,9 +112,9 @@ static int addNode(LinkedList* this, int nodeIndex,void* pElement) //size de la 
             Node* oldFirstNode = this->pFirstNode;
             this->pFirstNode = newNode;
             this->pFirstNode->pNextNode = oldFirstNode;
-                    newNode->pElement = pElement;
+            newNode->pElement = pElement;
             this->size = ll_len(this) + 1;
-        returnAux = 0;
+            returnAux = 0;
         }
         else if(this->pFirstNode != NULL && nodeIndex > 0 && nodeIndex < ll_len(this))//B
         {
@@ -123,9 +123,9 @@ static int addNode(LinkedList* this, int nodeIndex,void* pElement) //size de la 
             newNode->pNextNode = oldNodeInIndex;   //el nuevo nodo tiene como siguiente al ocupa
             Node* previousNode = getNode(this,nodeIndex - 1);  //traigo el nodo anterior a nodeIndex
             previousNode->pNextNode = newNode;   //el nodo anterior a nodeIndex  tiene como siguiente al nuevo
-                    newNode->pElement = pElement;
+            newNode->pElement = pElement;
             this->size = ll_len(this) + 1;
-        returnAux = 0;
+            returnAux = 0;
         }
         else if(this->pFirstNode != NULL && nodeIndex == ll_len(this))//C
         {
@@ -133,9 +133,9 @@ static int addNode(LinkedList* this, int nodeIndex,void* pElement) //size de la 
             Node* lastNode = getNode(this,nodeIndex - 1);//traigo al ultimo nodo
             lastNode->pNextNode = newNode;   //el ultimo nodo tiene como siguiente al nuevo
 
-                    newNode->pElement = pElement;
+            newNode->pElement = pElement;
             this->size = ll_len(this) + 1;
-        returnAux = 0;
+            returnAux = 0;
         }
 
 
@@ -169,6 +169,13 @@ int test_addNode(LinkedList* this, int nodeIndex,void* pElement)
 int ll_add(LinkedList* this, void* pElement)
 {
     int returnAux = -1;
+    int len = ll_len(this);
+
+    if(this!= NULL)
+    {
+        addNode(this,len,pElement);
+        returnAux = 0;
+    }
 
     return returnAux;
 }
@@ -184,6 +191,15 @@ int ll_add(LinkedList* this, void* pElement)
 void* ll_get(LinkedList* this, int index)
 {
     void* returnAux = NULL;
+    Node* nodoAux;
+    int len = ll_len(this);
+
+
+    if(this != NULL && index >= 0 && index < len && len > 0)
+    {
+        nodoAux = getNode(this,index);
+        returnAux = nodoAux->pElement;
+    }
 
     return returnAux;
 }
@@ -201,6 +217,15 @@ void* ll_get(LinkedList* this, int index)
 int ll_set(LinkedList* this, int index,void* pElement)
 {
     int returnAux = -1;
+    Node* nodoAux;
+    int len = ll_len(this);
+
+    if(this != NULL && index >= 0 && index < len && len > 0)
+    {
+        nodoAux = getNode(this,index);
+        nodoAux->pElement = pElement;
+        returnAux = 0;
+    }
 
     return returnAux;
 }
@@ -217,6 +242,32 @@ int ll_set(LinkedList* this, int index,void* pElement)
 int ll_remove(LinkedList* this,int index)
 {
     int returnAux = -1;
+
+    if(this != NULL && index >= 0 && index <= ll_len(this))
+    {
+       //estoy en condiciones de borrar un elemento
+       //para borrar un nodo tengo que preguntar en que posicion esta.
+        if(this->pFirstNode == NULL && index == 0)//D
+        {
+            //borro el primer nodo.
+            returnAux = 0;
+        }
+        else if(this->pFirstNode != NULL && index == 0)//A
+        {
+            //borro el nodo del principio de la lista
+            returnAux = 0;
+        }
+        else if(this->pFirstNode != NULL && index > 0 && index < ll_len(this))//B
+        {
+            //borro entre dos nodos
+            returnAux = 0;
+        }
+        else if(this->pFirstNode != NULL && index == ll_len(this))//C
+        {
+             //borro al final
+            returnAux = 0;
+        }
+    }
 
     return returnAux;
 }
