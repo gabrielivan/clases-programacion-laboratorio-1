1ef60dbf5c4c40c99379eb8740d490c0d95d5e0e
gabrielivan <40808771+gabrielivan@users.noreply.github.com>
2018-11-15 21:59:07 -0300

    Add files via upload

---
+++ clases repaso/EjercicioPre2doParcialV2/EjercicioPre2doParcialV2/Parser.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "LinkedList.h"
+#include "Empleado.h"
+
+#define BUFFER 2000
+
+int parser_parseEmpleados(char* fileName, LinkedList* listaEmpleados)
+{
+    int retorno = -1;
+    char* bufferId;
+    char* bufferName;
+    char* bufferHorasTrabajadas;
+    char bufferFile[BUFFER];
+    Empleado* pEmpleado;
+    FILE* auxFile = fopen(fileName,"r");
+
+    if(auxFile != NULL)
+    {
+        fgets(bufferFile,sizeof(bufferFile),auxFile);
+        //printf("%s",bufferFile);
+
+        if(bufferFile != NULL)
+        {
+            strtok(bufferFile,"\n");
+            while(!feof(auxFile))
+            {
+                fgets(bufferFile,sizeof(bufferFile),auxFile);
+                bufferId = strtok(bufferFile,",");
+                //printf("%s\n",bufferId);
+                bufferName = strtok(NULL,",");
+               // printf("%s\n",bufferName);
+                bufferHorasTrabajadas = strtok(NULL,";");
+                //printf("%s\n",bufferHorasTrabajadas);
+
+                pEmpleado = Empleado_newConParametros(bufferId,bufferName,bufferHorasTrabajadas,"1000");
+
+                if(pEmpleado != NULL)
+                {
+                    ll_add(listaEmpleados,pEmpleado);
+                    retorno = 0;
+                }
+            }
+        }
+    }
+    return retorno;
+
+}
+
+
